9bb93a833157f65769a9474589d084390689a983
Lan <lanlehoang10@gmail.com>
2019-05-11 00:57:23 +0700

    end sec6

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,17 +1,44 @@
 import React,{Component} from 'react';
-import {Container} from 'semantic-ui-react'
+import {Container} from 'semantic-ui-react';
+import {Route,Switch} from 'react-router-dom'
 import  EventDashboard  from "../../features/event/EventDashboard/EventDashboard";
 import NavBar from "../../features/nav/NavBar/NavBar";
+import EventDetailedPage from '../../features/event/EventDetailed/EventDetailedPage';
+import PeopleDashboard from '../../features/user/PeopleDashboard/PeopleDashboard';
+import UserDetailedPage from '../../features/user/UserDetailed/UserDetailedPage';
+import SettingsDashboard from '../../features/user/Settings/SettingsDashboard';
+import EventForm from '../../features/event/EventForm/EventForm';
+import HomePage from '../../features/home/HomePage';
 class App extends Component{
   render(){
     return (
-     <div>
-       <NavBar></NavBar>
-       <Container className="main">
-       
-       <EventDashboard></EventDashboard>
-       </Container>
-     </div>
+      <div>
+        <Switch>
+         <Route exact path='/' component={HomePage}></Route>
+         </Switch>
+         <Route path="/(.+)" render={()=>(
+           <div>
+          <NavBar></NavBar>
+        <Container className="main">
+        <Switch>
+          <Route path='/events' component={EventDashboard}></Route>
+          <Route path='/events/:id' component={EventDetailedPage}></Route>
+          <Route path='/people' component={PeopleDashboard}></Route>
+          <Route path='/profile/:id' component={UserDetailedPage}></Route>
+          <Route path='/settings' component={SettingsDashboard}></Route>
+          <Route path='/createEvent' component={EventForm}></Route>
+
+        </Switch>
+        
+        </Container>
+        </div>
+         )}></Route>
+        
+        
+  
+
+      </div>
+     
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
 const rootEl = document.getElementById('root');
 let render = () =>{
-    ReactDOM.render(<App/>,rootEl)
+    ReactDOM.render(
+    <BrowserRouter>
+      <App/>
+    </BrowserRouter>
+    ,rootEl)
 }
 if (module.hot){
     module.hot.accept('./app/layout/App',()=>{
